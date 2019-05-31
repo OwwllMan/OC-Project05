440acb0ed7694d74bfadc8029e993b8492c32194
OwwllMan <39001414+OwwllMan@users.noreply.github.com>
2019-06-01 01:01:26 +0200

    Générateur Ok , Citations Missing

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var citationPart2 = ["Citation2.1", "Citation2.2", "Citation2.3"];
 var citationPart3 = ["Citation3.1", "Citation3.2", "Citation3.3"];
 var auteurs = ["Auteur1", "Auteur2", "Auteur3"];
 
+var userNbText = 'Choissisez un nombre entre 1 et 6';
 
 
 $('document').ready(function(){
@@ -14,19 +15,147 @@ $('document').ready(function(){
 
 	$('#generer').click(function(){
 
-			aleaC1 = Math.floor(Math.random() * citationPart1.length);
-			aleaC2 = Math.floor(Math.random() * citationPart2.length);
-			aleaC3 = Math.floor(Math.random() * citationPart3.length);
+			userChose = prompt(userNbText);
+			if ((userChose < 0)  || (userChose <= 5)){
 
-			aleaA = Math.floor(Math.random() * auteurs.length);
+				console.log("L'utilisateur a choisit de générer " + userChose + " citation(s)");
 
-			var citation = citationPart1[aleaC1] + " " + citationPart2[aleaC2] + " " + citationPart3[aleaC3];
-			$('.citation').html(citation);
-			$('.auteur').html(auteurs[aleaA]);	
-	});
+				if (userChose == 1){
+						var arrayCita = [];
+						var arrayAut = [];
+						var i = 0;
+						while(i!=1){
+					       aleaC1 = Math.floor(Math.random() * citationPart1.length);
+					       aleaC2 = Math.floor(Math.random() * citationPart2.length);
+					       aleaC3 = Math.floor(Math.random() * citationPart3.length);
+					       aleaA = Math.floor(Math.random() * auteurs.length);
+					       var citation = citationPart1[aleaC1] + " " + citationPart2[aleaC2] + " " + citationPart3[aleaC3];
+					       var auteur = auteurs[aleaA];
 
-	$('#tweeter').click(function(){
-		window.open('https://twitter.com/intent/tweet?text=' + citations + " - " + auteurs +'&via=OC&hashtags=citation&url=https://google.com', 'twitter', 'height=300, width=550, scrollbars=0, menubar=0');
+					       arrayCita.push(citation)
+					       arrayAut.push(auteur)
+					       i++
+						}
+
+						$('.citation1').html(arrayCita[0]);
+						$('.auteur1').html(arrayAut[0]);
+
+
+				}else{}
+
+				if (userChose == 2){
+						var arrayCita = [];
+						var arrayAut = [];
+						var i = 0;
+						while(i!=2){
+					       aleaC1 = Math.floor(Math.random() * citationPart1.length);
+					       aleaC2 = Math.floor(Math.random() * citationPart2.length);
+					       aleaC3 = Math.floor(Math.random() * citationPart3.length);
+					       aleaA = Math.floor(Math.random() * auteurs.length);
+					       var citation = citationPart1[aleaC1] + " " + citationPart2[aleaC2] + " " + citationPart3[aleaC3];
+					       var auteur = auteurs[aleaA];
+
+					       arrayCita.push(citation)
+					       arrayAut.push(auteur)
+					       i++
+						}
+
+						$('.citation1').html(arrayCita[0]);
+						$('.citation2').html(arrayCita[1]);
+						$('.auteur1').html(arrayAut[0]);
+						$('.auteur2').html(arrayAut[1]);
+				}else{}
+
+				if (userChose == 3){
+						var arrayCita = [];
+						var arrayAut = [];
+						var i = 0;
+						while(i!=3){
+					       aleaC1 = Math.floor(Math.random() * citationPart1.length);
+					       aleaC2 = Math.floor(Math.random() * citationPart2.length);
+					       aleaC3 = Math.floor(Math.random() * citationPart3.length);
+					       aleaA = Math.floor(Math.random() * auteurs.length);
+					       var citation = citationPart1[aleaC1] + " " + citationPart2[aleaC2] + " " + citationPart3[aleaC3];
+					       var auteur = auteurs[aleaA];
+
+					       arrayCita.push(citation)
+					       arrayAut.push(auteur)
+					       i++
+						}
+
+						$('.citation1').html(arrayCita[0]);
+						$('.citation2').html(arrayCita[1]);
+						$('.citation3').html(arrayCita[2]);
+						$('.auteur1').html(arrayAut[0]);
+						$('.auteur2').html(arrayAut[1]);
+						$('.auteur3').html(arrayAut[2]);
+
+				}else{}
+
+				if (userChose == 4){
+						var arrayCita = [];
+						var arrayAut = [];
+						var i = 0;
+						while(i!=4){
+					       aleaC1 = Math.floor(Math.random() * citationPart1.length);
+					       aleaC2 = Math.floor(Math.random() * citationPart2.length);
+					       aleaC3 = Math.floor(Math.random() * citationPart3.length);
+					       aleaA = Math.floor(Math.random() * auteurs.length);
+					       var citation = citationPart1[aleaC1] + " " + citationPart2[aleaC2] + " " + citationPart3[aleaC3];
+					       var auteur = auteurs[aleaA];
+
+					       arrayCita.push(citation)
+					       arrayAut.push(auteur)
+					       i++
+						}
+
+						$('.citation1').html(arrayCita[0]);
+						$('.citation2').html(arrayCita[1]);
+						$('.citation3').html(arrayCita[2]);
+						$('.citation4').html(arrayCita[3]);
+						$('.auteur1').html(arrayAut[0]);
+						$('.auteur2').html(arrayAut[1]);
+						$('.auteur3').html(arrayAut[2]);
+						$('.auteur4').html(arrayAut[3]);
+
+				}else{}
+
+				if (userChose == 5){
+						var arrayCita = [];
+						var arrayAut = [];
+						var i = 0;
+						while(i!=5){
+					       aleaC1 = Math.floor(Math.random() * citationPart1.length);
+					       aleaC2 = Math.floor(Math.random() * citationPart2.length);
+					       aleaC3 = Math.floor(Math.random() * citationPart3.length);
+					       aleaA = Math.floor(Math.random() * auteurs.length);
+					       var citation = citationPart1[aleaC1] + " " + citationPart2[aleaC2] + " " + citationPart3[aleaC3];
+					       var auteur = auteurs[aleaA];
+
+					       arrayCita.push(citation)
+					       arrayAut.push(auteur)
+					       i++
+						}
+
+						$('.citation1').html(arrayCita[0]);
+						$('.citation2').html(arrayCita[1]);
+						$('.citation3').html(arrayCita[2]);
+						$('.citation4').html(arrayCita[3]);
+						$('.citation5').html(arrayCita[4]);
+						$('.auteur1').html(arrayAut[0]);
+						$('.auteur2').html(arrayAut[1]);
+						$('.auteur3').html(arrayAut[2]);
+						$('.auteur4').html(arrayAut[3]);
+						$('.auteur5').html(arrayAut[4]);
+							//Ouais je sais c'est pas super propre ^^
+				}else{}
+
+
+
+			} else {
+				alert("Merci de choisir un nombre entre 1 et 6");
+				console.log("L'utilisateur a choisit un mauvais nombre");
+			}
 
 	});
 
